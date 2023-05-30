4ef56764e213f63ea9d8de50fd955ce4532f6800
Haider <haideralirafeeq@gmail.com>
2023-05-30 18:32:08 +0500

    List of Taprize done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header/Header';
+import List_of_taprize_card from './Components/List_of_taprize_card/List_of_taprize_card';
+import List_f_Taprize from './Components/List_of_taprize_card/List_f_Taprize';
 
 function App() {
   return (
     <div className="App">
-     <Header/>
+     {/* <Header/> */}
+     {/* <List_of_taprize_card/> */}
+     <List_f_Taprize/>
     </div>
   );
 }
---
+++ src/Components/List_of_taprize_card/Data.js
@@ -0,0 +1,103 @@
+import pic_1 from '../Assets/Taprize materials/image 1.png'
+import pic_2 from '../Assets/Taprize materials/image 2.png'
+import pic_3 from '../Assets/Taprize materials/image 3.png'
+import pic_4 from '../Assets/Taprize materials/image 4.png'
+import pic_5 from '../Assets/Taprize materials/image 5.png'
+import pic_6 from '../Assets/Taprize materials/image 6.png'
+
+
+const Data = [
+  {
+    Image: pic_1,
+    Name: "近化ホキ店開い史集ヒ技写ちだリ回9和アヨエサ遊極レ",
+    tag: "発売中",
+  },
+  {
+    Image: pic_2,
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "販売終了",
+  },
+  {
+    Image: pic_3,
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "近日発売",
+  },
+  {
+    Image: pic_4,
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "発売中",
+  },
+  {
+    Image: pic_5,
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "販売終了",
+  },
+  {
+    Image: pic_6,
+    Name: "近化ホキ店開い史集ヒ技写ちだリ回9和アヨエサ遊極レ",
+    tag: "発売中",
+  },
+  {
+    Image: pic_1,
+    Name: "近化ホキ店開い史集ヒ技写ちだリ回9和アヨエサ遊極レ",
+    tag: "発売中",
+  },
+  {
+    Image: pic_2,
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "販売終了",
+  },
+  {
+    Image: pic_3,
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "近日発売",
+  },
+  {
+    Image: pic_4,
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "発売中",
+  },
+  {
+    Image: pic_5,
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "販売終了",
+  },
+  {
+    Image: pic_6,
+    Name: "近化ホキ店開い史集ヒ技写ちだリ回9和アヨエサ遊極レ",
+    tag: "発売中",
+  },
+  {
+    Image: pic_1,
+    Name: "近化ホキ店開い史集ヒ技写ちだリ回9和アヨエサ遊極レ",
+    tag: "発売中",
+  },
+  {
+    Image: pic_2,
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "販売終了",
+  },
+  {
+    Image: pic_3,
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "近日発売",
+  },
+  {
+    Image: pic_4,
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "発売中",
+  },
+  {
+    Image: pic_5,
+    Name: "台スマ境報コリホ直西時のご表74更ヘヤ無車のも",
+    tag: "販売終了",
+  },
+  {
+    Image: pic_6,
+    Name: "近化ホキ店開い史集ヒ技写ちだリ回9和アヨエサ遊極レ",
+    tag: "発売中",
+  },
+];
+
+export default Data;
+
