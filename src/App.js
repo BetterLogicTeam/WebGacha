850863cc6af38c2d544fb04176904a30d9105142
DevNawazishali <alinawazish150p@gmail.com>
2023-05-30 18:33:28 +0500

    notation and contact us pages made

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header/Header';
+import Notation from './Components/Notation/Notation';
+import Notation_table from './Components/Notation_table/Notation_table';
+import ContactUs from './Components/ContactUs/ContactUs';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Notation/>
+    {/* <Notation_table /> */}
+    <ContactUs />
+
     </div>
   );
 }
