061ccf03a348dfdbfa58a1473e7ee47b19e5e9d4
Haider <haideralirafeeq@gmail.com>
2023-06-02 13:15:34 +0500

    registration forms changes done

--- src/Components/Register_complete/Register_complete.jsx
+++ src/Components/Register_complete/Register_complete.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./Register_complete.css"
 import { useNavigate } from 'react-router-dom'
 
 export default function Register_complete() {
@@ -7,12 +8,12 @@ export default function Register_complete() {
     <div className='register_main_bg py-5'>
     <div className="container">
         <div className="row justify-content-center">
-            <div className="col-md-4">
+            <div className="col-md-6">
                 <div className="complete_content text-center">
                     <h3 className='h3_text'>登録完了</h3>
                     <p className='ptext'>会員登録が完了いたしました。ウェブガチャをお楽しみください。</p>
                     <p className='ptext'>登録内容を変更したい場合は下記マイページからお願いいたします</p>
-                    <button className='white_button' onClick={()=>history('/MyPageEdit1')}> 
+                    <button className='white_button veryLsBtn mt-2' onClick={()=>history('/MyPageEdit1')}> 
                     マイページへ
                     </button>
                 </div>
