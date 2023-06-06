ee6be1959abb71b29e86eb7e4e28a5d7e936f27a
BetterLogicTeam <47892812+tayyabarine@users.noreply.github.com>
2023-06-06 19:55:37 +0500

    Text Showd

--- src/Components/Landing_Page/Landing_Page.jsx
+++ src/Components/Landing_Page/Landing_Page.jsx
@@ -7,11 +7,11 @@ export default function   Landing_Page() {
         <div className="main_div_Landing">
           <div className="inner_text_landing">
             <h1>限定グッズが必ず当たる！</h1>
-            <h1> ウェブ版カプセルプライズ！</h1>
-            <p>
+            <h1> Web版カプセルプライズ！</h1>
+            <p className="mt-2">
               企画・製造・販売まで手掛けているから、ここにしかない商品をお届け。
             </p>
-            <div className="d-flex justify-content-center">
+            <div className="d-flex justify-content-center mt-1">
 
             <button>販売中の販売中のウェブガチャをチェック！</button>
             </div>
