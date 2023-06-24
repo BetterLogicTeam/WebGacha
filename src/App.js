4c77a85f9b384066b3a3d82f5ae4cffc3070b044
Hamza Khan <hamzakhan4991@gmail.com>
2023-06-24 22:49:01 +0500

    response

--- src/App.js
+++ src/App.js
@@ -68,10 +68,7 @@ function App() {
           path="/Text"
           element={<Text />}
         />
-        <Route
-          path="/ContactUs_Confirmation"
-          element={<MainConfirmationPage />}
-        />
+        <Route path="/ContactUs_Confirmation" element={<MainConfirmationPage />} />
       </Routes>
 
       <MainFooter />
